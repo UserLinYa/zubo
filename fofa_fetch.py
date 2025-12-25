d9e59c93e06921b6a5e87a98cde734bd12223140
UserLinYa <1941679649@qq.com>
2025-12-25 12:38:07 +0800

    Update fofa_fetch.py

--- fofa_fetch.py
+++ fofa_fetch.py
@@ -401,7 +401,7 @@ if __name__ == "__main__":
 
     run_count = first_stage()
 
-    if run_count % 10 == 0:
+    if run_count % 5 == 0:
         second_stage()
         third_stage()
 # ================================================插入文件内容 
@@ -426,5 +426,5 @@ if __name__ == "__main__":
         else:
             print("❌ IPTV文件不存在，跳过")
     else:
-        print("ℹ️ 本次不是 10 的倍数，跳过第二、三阶段")
+        print("ℹ️ 本次不是 05 的倍数，跳过第二、三阶段")
     push_all_files()
